a25394ee9b6dd57bcfaab87247d4d777538547ae
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-07-23 03:50:22 -0700

    I'm missing something, might need jquery for contact form as well as php

---
+++ assets/js/contact.js
@@ -0,0 +1,28 @@
+let valid = true 
+
+function contactFormValidation() {
+    let name = document.getElementById('formName')
+    let email = document.getElementById('formEmail')
+    let subject = document.getElementById('formSubject')
+    let message = document.getElementById('formMessage')
+
+    if (name == '*Name') {
+        valid = false
+    }
+    if (email == '*Email') {
+        valid = false
+    } 
+    if (subject == '*Subject') {
+        valid = false
+    }
+    if (message == '*Message') {
+        valid = false
+    }
+
+    if (valid != true) {
+        alert('Please fill out the form!')
+    }
+    else {
+        continue
+    } 
+}
